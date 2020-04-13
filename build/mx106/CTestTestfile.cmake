7e57e6e02004ace40e73f0d892403644403bdb71
Pripekashkin <densik554@gmail.com>
2020-04-13 14:28:38 +0300

    RvizEnd

---
+++ build/mx106/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/noreply/Mx106/src/mx106
+# Build directory: /home/noreply/Mx106/build/mx106
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
